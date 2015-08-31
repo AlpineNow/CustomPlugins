bbd1076e2c3e6c11b6899a358aeeeea046137c3b
alpinebuilder <alpine.builder.pivotal@gmail.com>
2015-08-30 21:42:50 -0700

    Updating the plugin samples from adl via Jenkins job publish-plugin-samples.

--- PluginExampleProject/src/main/scala/com/alpine/plugin/samples/ver1_0/DBNumericFeatureTransformer.scala
+++ PluginExampleProject/src/main/scala/com/alpine/plugin/samples/ver1_0/DBNumericFeatureTransformer.scala
@@ -14,19 +14,18 @@
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package com.alpine.plugin.samples.ver1_0
 
+import scala.collection.mutable
 
 import com.alpine.plugin.core._
 import com.alpine.plugin.core.datasource.OperatorDataSourceManager
+import com.alpine.plugin.core.db.{DBExecutionContext, DBRuntime}
 import com.alpine.plugin.core.dialog.{ColumnFilter, OperatorDialog}
 import com.alpine.plugin.core.io._
 import com.alpine.plugin.core.io.defaults.DBTableDefault
 import com.alpine.plugin.core.utils.DBParameterUtils
-
-import scala.collection.mutable
-import com.alpine.plugin.core.db.{DBExecutionContext, DBRuntime}
-
 
 class DBNumericFeatureTransformerSignature extends OperatorSignature[
   DBNumericFeatureTransformerGUINode,
